073d99fe29ddf935863f05da5ce21935dd2db9a1
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-07-19 05:21:55 +0300

    refactor: replace `validate-npm-package-name` with `zod` implementation
    This is direct port of [`validate-npm-package-name`](https://github.com/npm/validate-npm-package-name).

--- src/assert-valid-package-name.test.ts
+++ src/assert-valid-package-name.test.ts
@@ -1,8 +1,94 @@
 import { expect, test } from "vitest";
-import { assertValidPackageName } from "./assert-valid-package-name";
+import { assertStrictPackageName, assertValidPackageName } from "./assert-valid-package-name";
 
-test("assertValidPackageName", () => {
+test("basic package names", () => {
+	expect(() => assertValidPackageName("some-package")).not.toThrow();
+	expect(() => assertValidPackageName("example.com")).not.toThrow();
+	expect(() => assertValidPackageName("under_score")).not.toThrow();
+	expect(() => assertValidPackageName("period.js")).not.toThrow();
+	expect(() => assertValidPackageName("123numeric")).not.toThrow();
+	expect(() => assertValidPackageName("crazy!")).not.toThrow();
+});
+
+test("basic package names - strict", () => {
+	expect(() => assertStrictPackageName("some-package")).not.toThrow();
+	expect(() => assertStrictPackageName("example.com")).not.toThrow();
+	expect(() => assertStrictPackageName("under_score")).not.toThrow();
+	expect(() => assertStrictPackageName("period.js")).not.toThrow();
+	expect(() => assertStrictPackageName("123numeric")).not.toThrow();
+	expect(() => assertValidPackageName("crazy!")).not.toThrow();
+});
+
+test("scoped npm packages", () => {
+	expect(() => assertValidPackageName("@npm/thingy")).not.toThrow();
+	expect(() => assertStrictPackageName("@npm/thingy")).not.toThrow();
+});
+
+test("empty package name", () => {
 	expect(() => assertValidPackageName("")).toThrow();
-	expect(() => assertValidPackageName("foo")).not.toThrow();
-	expect(() => assertValidPackageName("@foo/bar")).not.toThrow();
+	expect(() => assertStrictPackageName("")).toThrow();
+});
+
+test("starting with dot, underscore", () => {
+	expect(() => assertValidPackageName(".start-with-period")).toThrow();
+	expect(() => assertStrictPackageName(".start-with-period")).toThrow();
+	expect(() => assertValidPackageName("_starts-with-underscore")).toThrow();
+	expect(() => assertStrictPackageName("_starts-with-underscore")).toThrow();
+});
+
+test("can only container URL friendly characters", () => {
+	expect(() => assertValidPackageName("npm:package")).toThrow();
+	expect(() => assertStrictPackageName("npm:package")).toThrow();
+	expect(() => assertValidPackageName("s/l/a/s/h/e/s")).toThrow();
+	expect(() => assertStrictPackageName("s/l/a/s/h/e/s")).toThrow();
+});
+
+test("cannot have leading or trailing space", () => {
+	expect(() => assertValidPackageName(" leading-space")).toThrow();
+	expect(() => assertStrictPackageName(" leading-space")).toThrow();
+	expect(() => assertValidPackageName("trailing-space ")).toThrow();
+	expect(() => assertStrictPackageName("trailing-space ")).toThrow();
+});
+
+test("cannot be a blacklisted name", () => {
+	expect(() => assertValidPackageName("node_modules")).toThrow();
+	expect(() => assertStrictPackageName("node_modules")).toThrow();
+	expect(() => assertValidPackageName("favicon.ico")).toThrow();
+	expect(() => assertStrictPackageName("favicon.ico")).toThrow();
+});
+
+test("old names can be node builtins", () => {
+	expect(() => assertValidPackageName("http")).not.toThrow();
+	expect(() => assertValidPackageName("process")).not.toThrow();
+});
+
+test("strict names cannot be node builtins", () => {
+	expect(() => assertStrictPackageName("http")).toThrow();
+	expect(() => assertStrictPackageName("process")).toThrow();
+});
+
+const LONGER_THAN_214 =
+	"ifyouwanttogetthesumoftwonumberswherethosetwonumbersarechosenbyfindingthelargestoftwooutofthreenumbersandsquaringthemwhichismultiplyingthembyitselfthenyoushouldinputthreenumbersintothisfunctionanditwilldothatforyou-";
+
+const SHORTER_THAN_214 =
+	"ifyouwanttogetthesumoftwonumberswherethosetwonumbersarechosenbyfindingthelargestoftwooutofthreenumbersandsquaringthemwhichismultiplyingthembyitselfthenyoushouldinputthreenumbersintothisfunctionanditwilldothatforyou";
+
+test("old package name can be longer than 214", () => {
+	expect(() => assertValidPackageName(SHORTER_THAN_214)).not.toThrow();
+	expect(() => assertValidPackageName(LONGER_THAN_214)).not.toThrow();
+});
+
+test("strict package name cannot be longer than 214", () => {
+	expect(() => assertStrictPackageName(SHORTER_THAN_214)).not.toThrow();
+	expect(() => assertStrictPackageName(LONGER_THAN_214)).toThrow();
+});
+
+test("old packages can have capital letters", () => {
+	expect(() => assertValidPackageName("ShorterThan214")).not.toThrow();
+	expect(() => assertValidPackageName("LONGER_THAN_214")).not.toThrow();
+});
+
+test("strict packages can have capital letters", () => {
+	expect(() => assertStrictPackageName("ShorterThan214")).toThrow();
+	expect(() => assertStrictPackageName("LONGER_THAN_214")).toThrow();
 });
--- src/assert-valid-package-name.ts
+++ src/assert-valid-package-name.ts
@@ -1,9 +1,121 @@
-import validatePackageName from "validate-npm-package-name";
+import { z } from "zod";
 
-export const assertValidPackageName = (name: string) => {
-	const { validForOldPackages, validForNewPackages, warnings, errors } = validatePackageName(name);
-	const isValid = validForOldPackages || validForNewPackages;
-	if (!isValid) {
-		throw new Error("invalid package name", { cause: { name, warnings, errors } });
-	}
-};
+/**
+ * Builtin modules for Node.js
+ */
+export const nodeBuiltinModules = [
+	"_http_agent",
+	"_http_client",
+	"_http_common",
+	"_http_incoming",
+	"_http_outgoing",
+	"_http_server",
+	"_stream_duplex",
+	"_stream_passthrough",
+	"_stream_readable",
+	"_stream_transform",
+	"_stream_wrap",
+	"_stream_writable",
+	"_tls_common",
+	"_tls_wrap",
+	"assert",
+	"assert/strict",
+	"async_hooks",
+	"buffer",
+	"child_process",
+	"cluster",
+	"console",
+	"constants",
+	"crypto",
+	"dgram",
+	"diagnostics_channel",
+	"dns",
+	"dns/promises",
+	"domain",
+	"events",
+	"fs",
+	"fs/promises",
+	"http",
+	"http2",
+	"https",
+	"inspector",
+	"inspector/promises",
+	"module",
+	"net",
+	"os",
+	"path",
+	"path/posix",
+	"path/win32",
+	"perf_hooks",
+	"process",
+	"punycode",
+	"querystring",
+	"readline",
+	"readline/promises",
+	"repl",
+	"stream",
+	"stream/consumers",
+	"stream/promises",
+	"stream/web",
+	"string_decoder",
+	"sys",
+	"timers",
+	"timers/promises",
+	"tls",
+	"trace_events",
+	"tty",
+	"url",
+	"util",
+	"util/types",
+	"v8",
+	"vm",
+	"wasi",
+	"worker_threads",
+	"zlib",
+];
+
+export const blacklistedNames = ["node_modules", "favicon.ico"];
+
+export const NpmPackageName = z
+	.string()
+	.min(1)
+	.refine(name => !name.match(/^\./), "Package name cannot start with a leading period.")
+	.refine(name => !name.match(/^_/), "Package name cannot start with a leading underscore.")
+	.refine(name => name.trim() === name, "Package cannot contain leading or trailing spaces.")
+	.refine(
+		name => !blacklistedNames.includes(name.toLowerCase()),
+		"Package name cannot be node_modules or favicon.ico.",
+	)
+	.refine(name => {
+		const match = name.match(/^(?:@([^/]+?)[/])?([^/]+?)$/);
+		if (!match) {
+			return false;
+		}
+		const [_match, user, pkg] = match;
+		if (user && encodeURIComponent(user.toString()) !== user) {
+			return false;
+		}
+		if (pkg && encodeURIComponent(pkg.toString()) !== pkg) {
+			return false;
+		}
+		return true;
+	}, "Package name can only contain URL-friendly characters.");
+
+export function assertValidPackageName(name: unknown) {
+	NpmPackageName.parse(name);
+}
+
+export const StrictNpmPackageName = NpmPackageName.refine(
+	name => !nodeBuiltinModules.includes(name.toLowerCase()),
+	"Package name cannot be node_modules or favicon.ico.",
+)
+	.refine(name => name.length <= 214, "Package name cannot be longer than 214 characters.")
+	.refine(name => name === name.toLowerCase(), "Package name should be lower case.")
+	.refine(
+		name => !name.match(/(\[|~|'|!|\(|\)|\*|\])/),
+		"Package name cannot container special characters.",
+	);
+
+export function assertStrictPackageName(name: unknown) {
+	StrictNpmPackageName.parse(name);
+}
