3582b062465987c918431b1e4dba318ca94efa36
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-07-19 22:50:31 +0300

    feat: add test for `Client.fetch`

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,37 @@
-import { expect, test } from "vitest";
-import * as all from "./index";
+import { ofetch } from "ofetch";
+import { expect, test, vi } from "vitest";
+import { z } from "zod";
+import { Client } from ".";
+import { createCache } from "./cache";
 
-test("index", () => {
-	expect(all).toBeDefined();
+vi.mock("ofetch", async importOriginal => {
+	return {
+		...(await importOriginal<typeof import("ofetch")>()),
+		// this will only affect "foo" outside of the original module
+		ofetch: vi.fn(() => ({ foo: "bar" })),
+	};
+});
+
+test("fetchData", async () => {
+	const cache = createCache();
+	const client = new Client({
+		cache,
+	});
+	expect(cache.storage.getKeys()).resolves.toHaveLength(0);
+	expect(await client.fetch(z.object({ foo: z.string() }), "https://example.com"))
+		.toMatchInlineSnapshot(`
+		{
+		  "foo": "bar",
+		}
+	`);
+	expect(ofetch).toBeCalledTimes(1);
+	expect(cache.storage.getKeys()).resolves.toHaveLength(1);
+	expect(await client.fetch(z.object({ foo: z.string() }), "https://example.com"))
+		.toMatchInlineSnapshot(`
+		{
+		  "foo": "bar",
+		}
+	`);
+	expect(ofetch).toBeCalledTimes(1);
+	expect(cache.storage.getKeys()).resolves.toHaveLength(1);
 });
